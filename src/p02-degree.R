95629efffe0be89f711279a35d72e8a4d7e827ed
Vincent Labatut <vince.labatut@gmail.com>
2013-07-04 10:05:18 +0300

    fixed p02-degree

--- src/p02-degree.R
+++ src/p02-degree.R
@@ -1,56 +1,110 @@
 # Processes various calculations on the degree.
-# v1
 # 
+# Version: 1
 # Author: Vincent Labatut 06/2013
+#
 # source("C:/Eclipse/workspaces/Networks/Orleans/src/main-degree.R")
 # source("/home/vlabatut/eclipse/workspaces/Networks/Orleans/src/main-degree.R")
 ###############################################################################
+library("igraph")
+source("src/ecdflt.R")
 
-# data folder 					#TODO update depending on local file system
-#folder.data <- "C:/Eclipse/workspaces/Networks/Orleans/data/"	
-folder.data <- "/home/vlabatut/eclipse/workspaces/Networks/Orleans/data/"	
 
-# load data
-cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] Load raw data\n",sep="")
-data.file <- paste(folder.data,"data.txt",sep="")
-data <- as.matrix(read.table(data.file))
+###############################################################################
+# setup files
+###############################################################################
+folder.data <- "data/"	
+file.input <- "degrees.txt"		# TODO you can possibly change that
+degree.names <- c(							# TODO you might change that, if necessary
+		"out", "in", "all")
 
-# clean data
-cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] Clean data\n",sep="")
-for(c in 1:ncol(data))
-{	# get rid of NA (?)
-	cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..",c,": Remove NA symbols\n",sep="")
-	idx.na <- which(is.na(data[,c]))
-	if(length(idx.na)>0)
-		data[idx.na,c] <- 0
-	
-	# get rid of infinite values
-	cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..",c,": Remove infinite symbols\n",sep="")
-	idx.inf <- which(is.infinite(data[,c]))
-	if(length(idx.inf)>0)
-		data[idx.inf,c] <- 0
-}
 
-# load degree by cluster
-data <- cbind(data, matrix(NA,ncol=5,nrow=nrow(data)))
-cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] Load degree by cluster\n",sep="")
-for(i in 0:5)
-{	cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..Load cluster ",i," file\n",sep="")
-	cluster.file <- paste(folder.data,"Cluster",i,"_degrees.txt",sep="")
-	t <- read.table(cluster.file,sep=";")
-	deg <- as.matrix(t)
-	idx <- deg[,1] + 1
-	data[idx,(ncol(data)-4):(ncol(data)-3)] <- deg[,2:3]
-	data[idx,ncol(data)-2] <- deg[,2] + deg[,3]
-	data[idx,(ncol(data)-1):ncol(data)] <- deg[,4:5]
-}
+###############################################################################
+# load degree data
+###############################################################################
+start.time <- Sys.time();
+cat("[",format(start.time,"%a %d %b %Y %X"),"] Loading degree data\n",sep="")
+	# load the data
+	file.deg <- paste(folder.data,file.input,sep="")
+	degrees <- as.matrix(read.table(file.deg))
+	# possibly add the total degree, if missing
+	if(ncol(degrees)==2)
+		degrees <- cbind(degrees,degrees[,1]+degrees[,2])
+end.time <- Sys.time();
+total.time <- end.time - start.time;
+cat("[",format(end.time,"%a %d %b %Y %X"),"] Load completed in ",total.time,"\n",sep="")
 
-# names
-names <- c("id","community","intensity-int-out","intensity-int-in","diversity-out","diversity-in","intensity-ext-out","intensity-ext-in","homogeneity-out","homogeneity-in","degree-all","degree-out","degree-in","ratio","overlap")
-colnames(data) <- names
 
-# process correlations
-cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] Process degrees vs. measures correlations\n",sep="")
-cor.file <- paste(folder.data,"correlations.txt",sep="")
-cor.mat <- cor(data[,3:10],data[,11:13])
+###############################################################################
+# plot degree distributions
+###############################################################################
+start.time <- Sys.time();
+cat("[",format(start.time,"%a %d %b %Y %X"),"] Plot degree distributions\n",sep="")
+	for(i in 1:ncol(degrees))
+	{	deg.name <- paste(degree.names[i],"-degree",sep="")
+		
+		# histogram
+		plot.file <- paste(folder.data,"degree.",degree.names[i],".histo.pdf",sep="")
+		cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..Plot ",deg.name," histogram in file ",plot.file,"\n",sep="")
+		pdf(file=plot.file, bg="white")
+		hist(degrees[,i],probability=TRUE,breaks=100,main=paste("Distribution of",deg.name),xlab=deg.name,col="RED")
+		dev.off()
+		
+		# cumulative distribution
+		plot.file <- paste(folder.data,"degree.",degree.names[i],".cumdist.pdf",sep="")
+		cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..Plot ",deg.name," cumulative distribution in file ",plot.file,"\n",sep="")
+		pdf(file=plot.file, bg="white")
+		ecdflt(x=degrees[,i], xlab=deg.name, main=paste("Distribution of",deg.name), col="RED") #, log="y"
+		dev.off()
+	}
+end.time <- Sys.time();
+total.time <- end.time - start.time;
+cat("[",format(end.time,"%a %d %b %Y %X"),"] Plotting completed in ",total.time,"\n",sep="")
+
+
+###############################################################################
+# load raw data
+###############################################################################
+start.time <- Sys.time();
+cat("[",format(start.time,"%a %d %b %Y %X"),"] Loading raw data\n",sep="")
+	file.data <- paste(folder.data,file.input,sep="")
+	data <- as.matrix(read.table(file.data))
+end.time <- Sys.time();
+total.time <- end.time - start.time;
+cat("[",format(end.time,"%a %d %b %Y %X"),"] Load completed in ",total.time,"\n",sep="")
+
+
+###############################################################################
+# clean raw data
+###############################################################################
+start.time <- Sys.time();
+cat("[",format(start.time,"%a %d %b %Y %X"),"] Cleaning data\n",sep="")
+	for(c in 1:ncol(data))
+	{	cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ..Processing col.",c,"\n",sep="")
+		
+		# get rid of NA (?)
+		idx.na <- which(is.na(data[,c]))
+		if(length(idx.na)>0)
+		{	data[idx.na,c] <- 0
+			cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ....WARNING: ",length(idx.na)," NA values found in the raw data (col.",c,")\n")
+		}
+		
+		# get rid of infinite values
+		idx.inf <- which(is.infinite(data[,c]) | data[,c]==1.79769e+308)
+		if(length(idx.inf)>0)
+		{	data[idx.inf,c] <- 0
+			cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] ....Replacing ",length(idx.inf)," infinite values by 0 in col.",c,")\n")
+		}
+	}
+end.time <- Sys.time();
+total.time <- end.time - start.time;
+cat("[",format(end.time,"%a %d %b %Y %X"),"] Cleaning completed in ",total.time,"\n",sep="")
+
+
+###############################################################################
+# process degree vs. measure correlations
+###############################################################################
+cat("[",format(Sys.time(),"%a %d %b %Y %X"),"] Process and record correlations between measures\n",sep="")
+cor.mat <- cor(degrees, data)
+cor.file <- paste(folder.data,"degrees-measures.correlations.txt",sep="")
 write.table(cor.mat,cor.file,row.names=FALSE,col.names=FALSE)
