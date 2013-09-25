0e09750dc789887ca35cee8e2fd71898b394f5cf
Vincent Labatut <vince.labatut@gmail.com>
2013-09-25 11:35:38 +0300

    correction in degree script

--- src/p01-degree.R
+++ src/p01-degree.R
@@ -15,13 +15,13 @@ source("src/ecdflt.R")
 # setup files
 ###############################################################################
 folder.data <- "data/"
-file.input.soccap <- "degrees.txt"				# TODO you can possibly change that
-file.input.rolemeas <- "rolemeasures.raw.txt"		# TODO you can possibly change that
-rolemeas.names <- c(						# TODO you might change that, if necessary
+file.input.degrees <- "degrees.txt"				# TODO you can possibly change that
+file.input.rolemeas <- "rolemeasures.raw.txt"	# TODO you can possibly change that
+rolemeas.names <- c(							# TODO you might change that, if necessary
 		"intensity-int-out","intensity-int-in","diversity-out","diversity-in","intensity-ext-out","intensity-ext-in","homogeneity-out","homogeneity-in")
-degree.names <- c(							# TODO you might change that, if necessary
+degree.names <- c(								# TODO you might change that, if necessary
 		"out", "in", "all")
-sample.size <- 100000						# TODO processing the whole dataset is to long, so the power-law distribution is tested only on a sample
+sample.size <- 100000							# TODO processing the whole dataset is to long, so the power-law distribution is tested only on a sample
 
 
 ###############################################################################
@@ -30,11 +30,11 @@ sample.size <- 100000						# TODO processing the whole dataset is to long, so th
 start.time <- Sys.time();
 cat("[",format(start.time,"%a %d %b %Y %H:%M:%S"),"] Loading degree data\n",sep="")
 	# load the data
-	file.soccap <- paste(folder.data,file.input.soccap,sep="")
-	soccap.indices <- as.matrix(read.table(file.soccap))
+	file.degrees <- paste(folder.data,file.input.degrees,sep="")
+	degrees <- as.matrix(read.table(file.degrees))
 	# possibly add the total degree, if missing
-	if(ncol(soccap.indices)==2)
-		soccap.indices <- cbind(soccap.indices,soccap.indices[,1]+soccap.indices[,2])
+	if(ncol(degrees)==2)
+		degrees <- cbind(degrees,degrees[,1]+degrees[,2])
 end.time <- Sys.time();
 total.time <- end.time - start.time;
 cat("[",format(end.time,"%a %d %b %Y %H:%M:%S"),"] Load completed in ",total.time,"\n",sep="")
@@ -44,7 +44,7 @@ cat("[",format(end.time,"%a %d %b %Y %H:%M:%S"),"] Load completed in ",total.tim
 # sample a few objects
 ###############################################################################
 cat("[",format(Sys.time(),"%a %d %b %Y %H:%M:%S"),"] Sample ",sample.size," objects\n",sep="")
-sampled <- sample(x=1:nrow(soccap.indices),size=sample.size)
+sampled <- sample(x=1:nrow(degrees),size=sample.size)
 
 
 ###############################################################################
@@ -52,21 +52,21 @@ sampled <- sample(x=1:nrow(soccap.indices),size=sample.size)
 ###############################################################################
 start.time <- Sys.time();
 cat("[",format(start.time,"%a %d %b %Y %H:%M:%S"),"] Plot degree distributions\n",sep="")
-	for(i in 1:ncol(soccap.indices))
+	for(i in 1:ncol(degrees))
 	{	deg.name <- paste(degree.names[i],"-degree",sep="")
 		
 		# histogram
 		plot.file <- paste(folder.data,"degree-",degree.names[i],".histo.pdf",sep="")
 		cat("[",format(Sys.time(),"%a %d %b %Y %H:%M:%S"),"] ..Plot ",deg.name," histogram in file ",plot.file,"\n",sep="")
 		pdf(file=plot.file, bg="white")
-		hist(soccap.indices[,i],probability=TRUE,breaks=100,main=paste("Distribution of",deg.name),xlab=deg.name,col="RED")
+		hist(degrees[,i],probability=TRUE,breaks=100,main=paste("Distribution of",deg.name),xlab=deg.name,col="RED")
 		dev.off()
 		
 		# (partial) cumulative distribution
 		plot.file <- paste(folder.data,"degree-",degree.names[i],".cumdist.pdf",sep="")
 		cat("[",format(Sys.time(),"%a %d %b %Y %H:%M:%S"),"] ..Plot ",deg.name," cumulative distribution in file ",plot.file,"\n",sep="")
 		pdf(file=plot.file, bg="white")
-		ecdflt(x=soccap.indices[sampled,i], xlab=deg.name, main=paste("Complementary Cumulative Distribution of",deg.name), log="y", complementary=TRUE, col="RED", points=1000) #
+		ecdflt(x=degrees[sampled,i], xlab=deg.name, main=paste("Complementary Cumulative Distribution of",deg.name), log="y", complementary=TRUE, col="RED", points=1000) #
 		dev.off()
 	}
 end.time <- Sys.time();
@@ -117,7 +117,7 @@ cat("[",format(end.time,"%a %d %b %Y %H:%M:%S"),"] Cleaning completed in ",total
 # process degree vs. role measure correlations
 ###############################################################################
 cat("[",format(Sys.time(),"%a %d %b %Y %H:%M:%S"),"] Process and record correlations between degrees and role measures\n",sep="")
-cor.mat <- cor(soccap.indices, data)
+cor.mat <- cor(degrees, data)
 rownames(cor.mat) <- degree.names
 colnames(cor.mat) <- rolemeas.names
 cor.file <- paste(folder.data,"degrees.vs.rolemeasures.correlations.txt",sep="")
@@ -129,14 +129,14 @@ print(cor.mat)
 # plot degrees vs. role measures
 ###############################################################################
 cat("[",format(Sys.time(),"%a %d %b %Y %H:%M:%S"),"] Ploting role measures vs. degrees\n",sep="")
-for(i in 1:ncol(soccap.indices))
+for(i in 1:ncol(degrees))
 {	deg.name <- paste("degree-",degree.names[i],sep="")
 	
 	for(j in 1:ncol(data))
 	{	cat("[",format(Sys.time(),"%a %d %b %Y %H:%M:%S"),"] Ploting ",rolemeas.names[j]," vs. ",deg.name,"\n",sep="")
 		plot.file <- paste(folder.data,deg.name,".vs.",rolemeas.names[j],".pdf",sep="")
 		pdf(file=plot.file, bg="white")
-		plot(soccap.indices[sampled,i],data[sampled,j],main=paste(rolemeas.names[j],"vs.",deg.name),xlab=deg.name,ylab=rolemeas.names[j],col="RED")
+		plot(degrees[sampled,i],data[sampled,j],main=paste(rolemeas.names[j],"vs.",deg.name),xlab=deg.name,ylab=rolemeas.names[j],col="RED")
 		dev.off()
 	}
 }
